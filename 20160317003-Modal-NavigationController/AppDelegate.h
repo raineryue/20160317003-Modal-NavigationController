58a83feff6bb705871ead4d9bbef1435d3cad395
raineryue <it_jinlei.yue@163.com>
2016-03-17 16:32:27 +0800

    Initial Commit

---
+++ 20160317003-Modal-NavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160317003-Modal-NavigationController
+//
+//  Created by Rainer on 16/3/17.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160317003-Modal-NavigationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160317003-Modal-NavigationController
+//
+//  Created by Rainer on 16/3/17.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
